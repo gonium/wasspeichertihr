b244978365d4f2710c495910369c7a166485a929
Mathias Dalheimer <md@gonium.net>
2011-05-22 00:21:49 +0200

    added debug foo

--- js/applogic.js
+++ js/applogic.js
@@ -1,8 +1,8 @@
+var datum = document.getElementById('datum');
+var fristdatum = document.getElementById('fristdatum');
 var absendername = document.getElementById('absendername');
 var absenderstrasse = document.getElementById('absenderstrasse');
 var absenderort = document.getElementById('absenderort');
-var datum = document.getElementById('datum');
-var fristdatum = document.getElementById('fristdatum');
 
 function updateLetter(){
   unterschrift = document.getElementById('unterschrift');
@@ -13,6 +13,44 @@ function updateLetter(){
     document.getElementById('absenderstrasse').innerHTML + " " +
     document.getElementById('absenderort').innerHTML
 };
+
+function loadHook (){
+    alert("loadhook");
+    alert(localStorage.getItem('absendername'));
+  // initial updates after page load
+  if (localStorage.getItem('absendername')) {
+    var value = localStorage.getItem('absendername');
+    alert("Loaded from localstore: " + value);
+    absendername.innerHTML = value;
+  }
+  if (localStorage.getItem('absenderstrasse')) {
+    var value = localStorage.getItem('absenderstrasse');
+    alert("Loaded from localstore: " + value);
+    absenderstrasse.innerHTML = value;
+  }
+  if (localStorage.getItem('absenderort')) {
+    var value = localStorage.getItem('absenderort');
+    alert("Loaded from localstore: " + value);
+    absenderort.innerHTML = value;
+  }
+
+  updateLetter();
+
+  var jetzt = new Date();
+  var tag = jetzt.getDate();
+  var monat = jetzt.getMonth()+1;
+  var jahr = jetzt.getFullYear();
+  datum.innerHTML = tag + "." + monat + "." + jahr;
+  //var ms = jetzt.getMilliseconds() + 1000*60*60*24*14
+  var frist = new Date(jetzt.getTime() + 1000*60*60*24*14 );
+  var ftag = frist.getDate();
+  var fmonat = frist.getMonth()+1;
+  var fjahr = frist.getFullYear();
+  fristdatum.innerHTML = ftag + "." + fmonat + "." + fjahr;
+}
+// window.load
+
+addEvent(window, 'load', loadHook);
 
 // Absendername
 addEvent(absendername, 'blur', function () {
@@ -44,27 +82,4 @@ addEvent(absenderort, 'focus', function () {
   document.designMode = 'on';
 });
 
-// initial updates after page load
-if (localStorage.getItem('absendername')) {
-  absendername.innerHTML = localStorage.getItem('absendername');
-}
-if (localStorage.getItem('absenderstrasse')) {
-  absenderstrasse.innerHTML = localStorage.getItem('absenderstrasse');
-}
-if (localStorage.getItem('absenderort')) {
-  absenderort.innerHTML = localStorage.getItem('absenderort');
-}
 
-updateLetter();
-
-var jetzt = new Date();
-var tag = jetzt.getDate();
-var monat = jetzt.getMonth()+1;
-var jahr = jetzt.getFullYear();
-datum.innerHTML = tag + "." + monat + "." + jahr;
-//var ms = jetzt.getMilliseconds() + 1000*60*60*24*14
-var frist = new Date(jetzt.getTime() + 1000*60*60*24*14 );
-var ftag = frist.getDate();
-var fmonat = frist.getMonth()+1;
-var fjahr = frist.getFullYear();
-fristdatum.innerHTML = ftag + "." + fmonat + "." + fjahr;
